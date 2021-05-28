ed5b6333bd0b2b1fd9da1f925c6bfa7682299162
TecuAndreea <tecu_andreea@yahoo.com>
2021-05-28 18:09:57 +0300

    Recreated the WPF client project + provider

---
+++ ChatClient/ChatClientProvider/Services/GrpcChatServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using ChatService.Protos;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatClientProvider.Services
+{
+    class GrpcChatServiceProvider
+    {
+        private string Url { get; set; }
+        private Lazy<GrpcChannel> RpcChannel { get; set; }
+        private Chat.ChatClient ChatClient { get; set; }
+
+        public GrpcChatServiceProvider()
+        {
+            this.Url = "https://localhost:5001";
+            this.RpcChannel = new Lazy<GrpcChannel>(GrpcChannel.ForAddress(this.Url));
+        }
+
+        public Chat.ChatClient GetChatClient() => this.ChatClient ??= new Chat.ChatClient(this.RpcChannel.Value);
+
+    }
+}
